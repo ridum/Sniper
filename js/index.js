58b8c84aa66a37105492b13b8c718a3a5a370ef2
ridum <ridum1996@gmail.com>
2017-10-29 17:15:25 -0700

    bugfixed

--- js/index.js
+++ js/index.js
@@ -12,10 +12,8 @@ $.ajaxPrefilter(function (options) {
     if (options.crossDomain && jQuery.support.cors) {
         var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
         options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-        //options.url = "http://cors.corsproxy.io/url=" + options.url;
     }
 })
-// JSON.stringify({}) === '{}' // check if json object is null
 
 function wordMappinng(string, list) {
     //filter all period
@@ -41,34 +39,37 @@ function wordMappinng(string, list) {
 
 $(function () {
     var keyList = ["java", "c++", "javascript"];
-    var url = 'https://www.glassdoor.ca/job-listing/ixp-intern-software-developer-sap-JV_IC2278756_KO0,29_KE30,33.htm?jl=2562254803&ctt=1509053712570';
-    var text;
-
+    var lowerCaseKeyList = keyList.map(function (x) {
+        return x.toLowerCase();
+    });
     $("#analyze_button").click(function () {
-        var url = (isUrl) ? $("#urlText").val() : $("#pureText").val();
-        $.get(
-            url,
-            function (response) {
-                document.getElementById("mainContent").innerHTML = response;
+        var input = (isUrl) ? $("#urlText").val() : $("#pureText").val();
+        if (isUrl) {
+            $.get(
+                input,
+                function (response) {
+                    document.getElementById("mainContent").innerHTML = response;
 
-                //store the context
-                text = document.getElementById("mainContent").innerText.toLowerCase();
+                    //store the context
+                    var URLText = document.getElementById("mainContent").innerText.toLowerCase();
 
-                // remove the content to keep local css
-                $("#mainContent").remove();
+                    // remove the content to preserve local css
+                    $("#mainContent").remove();
 
-                //concert anything to lower case to compare
-                var lowerCaseKeyList = keyList.map(function (x) {
-                    return x.toLowerCase();
+                    //concert anything to lower case to compare
+
+                    var result = wordMappinng(URLText, lowerCaseKeyList);
+                    console.log(result);
+
+                }).done(function () {
+                    console.log("parse success");
+                }).fail(function () {
+                    alert("cannot aceess the file, try use input column");
                 });
-                var result = wordMappinng(text, lowerCaseKeyList);
-                console.log(result);
-
-            }).done(function () {
-                console.log("parse success");
-            }).fail(function () {
-                alert("cannot aceess the file, try use input column");
-            });
+        } else {
+            var result = wordMappinng(input, lowerCaseKeyList);
+            console.log(result);
+        }
     });
 
 });
