95f7190fcd193bd8b9d8b55707187b2e1b77a453
Shuoyi Ma <mashuoyi111@sina.com>
2017-11-09 16:52:44 -0800

    added generateSkillList

--- js/IOControl.js
+++ js/IOControl.js
@@ -1,3 +1,5 @@
+const SKILL_ROW_HTML = "<input placeholder='Skill Name' class='form-control skillName' onfocus='moveColumnToLeft()' onfocusout='resetColumn()'><textarea placeholder='Skill Description' class='skillTextArea form-control' onfocus='moveColumnToLeft()' onfocusout='resetColumn()' rows='4'></textarea><button class='btn btn-md btn-danger' onclick='removeSkill(this)'>delete</button>";
+
 function pressUrlButton() {
     if (!isUrl) {
         isUrl = true;
@@ -42,7 +44,7 @@ function createSkill() {
     var list = document.createElement("li");
     list.style.display = "none";
     list.className = "list-group-item";
-    list.innerHTML = "<input placeholder='Skill Name' class='form-control skillName' onfocus='moveColumnToLeft()' onfocusout='resetColumn()'><textarea placeholder='Skill Description' class='skillTextArea form-control' onfocus='moveColumnToLeft()' onfocusout='resetColumn()' rows='4'></textarea><button class='btn btn-md btn-danger' onclick='removeSkill(this)'>delete</button>";
+    list.innerHTML = SKILL_ROW_HTML;
     document.getElementById("skillList").appendChild(list);
     $(list).fadeIn(1000);
 }
@@ -106,3 +108,24 @@ function putOnTextArea(text) {
     var resultDiv = document.getElementById("resultText");
     resultDiv.value = text;
 }
+
+function generateSkillList(list) {
+    var skillList = document.getElementById("skillList")
+    skillList.innerHTML = "";
+    skillList.style.display = "none";
+    var newSkillList = "";
+    for(let i=0; i<list.length; i++){
+        let list = document.createElement("li");
+        list.style.display = "none";
+        list.className = "list-group-item";
+        list.innerHTML = SKILL_ROW_HTML;
+        skillList.appendChild(list);
+    }
+    list.foreach((ele, index) =>{
+        $(skillList.children[index]).children(".skillName").val(ele.name);
+        $(skillList.children[index]).children(".skillTextArea").val(ele.descr);
+    });
+    $(skillList).fadeIn(1000);
+}
+
+
